18e9563141e1e7f6c1133668ca15e1c5af19fc0d
Yurii <predbannikov.yurii@gmail.com>
2021-03-20 19:30:51 +0300

    added smth word pairs

--- questions.cpp
+++ questions.cpp
@@ -9,6 +9,47 @@ Questions::Questions()
     questList.append(TwoWords("when", "когда"));
     questList.append(TwoWords("why", "почему, зачем"));
     questList.append(TwoWords("how", "как, каким образом"));
+    questList.append(TwoWords("me", "меня, мне"));
+    questList.append(TwoWords("my", "мой"));
+    questList.append(TwoWords("your", "твой"));
+    questList.append(TwoWords("his", "его"));
+    questList.append(TwoWords("him", "его, ему"));
+    questList.append(TwoWords("her", "её, ей"));
+    questList.append(TwoWords("us", "нас, нам"));
+    questList.append(TwoWords("our", "наш"));
+    questList.append(TwoWords("them", "их, им"));
+    questList.append(TwoWords("their", "их"));
+    questList.append(TwoWords("here", "здесь, сюда"));
+    questList.append(TwoWords("there", "там, туда"));
+    questList.append(TwoWords("this", "этот"));
+    questList.append(TwoWords("that", "тот"));
+    questList.append(TwoWords("these", "эти"));
+    questList.append(TwoWords("those", "те"));
+    questList.append(TwoWords("such a", "такой"));
+    questList.append(TwoWords("such", "такие"));
+    questList.append(TwoWords("the same", "тот самый, те самые"));
+    questList.append(TwoWords("another", "другой"));
+    questList.append(TwoWords("i mean", "я полагаю"));
+    questList.append(TwoWords("sort of", "вроде, как бы"));
+    questList.append(TwoWords("like", "похожий"));
+
+    questList.append(TwoWords("we regret", "мы сожалеем"));
+    questList.append(TwoWords("we apologize", "мы приносим извенения"));
+
+    questList.append(TwoWords("good morning", "доброе утро"));
+    questList.append(TwoWords("good afternoon", "добрый день"));
+    questList.append(TwoWords("good evening", "добрый вечер"));
+    questList.append(TwoWords("good night", "доброй ночи"));
+
+    questList.append(TwoWords("so long", "пока"));
+    questList.append(TwoWords("farewell", "прощайте"));
+
+    questList.append(TwoWords("old", "старый"));
+    questList.append(TwoWords("older", "старше"));
+    questList.append(TwoWords("the oldest", "самый старый"));
+    questList.append(TwoWords("more", "более"));
+    questList.append(TwoWords("most likely", "скорее всего"));
+
 
 }
 
